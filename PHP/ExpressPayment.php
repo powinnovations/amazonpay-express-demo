44c96d834caa16f084480d993680783d48943191
shrakuma <shrakuma@amazon.com>
2015-01-14 16:57:55 -0800

    PHP initial commit

---
+++ PHP/ExpressPayment.php
@@ -0,0 +1,59 @@
+
+<?php
+/* create token to prevent cross-site request forgery */
+session_start();
+$_SESSION["token"] = md5(uniqid(mt_rand(), true));
+?>
+<html>
+
+<head>
+    <script type='text/javascript' src="https://static-na.payments-amazon.com/OffAmazonPayments/us/sandbox/js/Widgets.js"></script>
+    <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+
+    <style>
+        label,
+        div {
+            margin: 10px;
+        }
+    </style>
+</head>
+
+<body>
+    <input type="hidden" name="csrf" id="csrf" value="<?php echo $_SESSION["token"]; ?>">
+    <label id="itemname" for="tshirt">Item Name: Long Sleeve Tee</label>
+    <div id="amount" value="100">Price: $100</div>
+
+    <label for="QuantitySelect">Qty:</label>
+    <select id="QuantitySelect">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+    </select>
+
+    <div id="AmazonPayButton"></div>
+
+    <script type="text/javascript">
+        OffAmazonPayments.Button("AmazonPayButton", "YOUR_SELLER_ID_HERE", {
+
+            type: "hostedPayment",
+
+            hostedParametersProvider: function(done) {
+
+                $.getJSON("signature_express.php", {
+                    amount: parseInt($("#amount").attr("value")) * parseInt($("#QuantitySelect option:selected").val()),
+                    currencyCode: 'USD',
+                    sellerNote: $("#itemname").text() + ' QTY: ' + $("#QuantitySelect option:selected").val(),
+                    csrf:$("#csrf").val()
+
+                }, function(data) {
+                    done(data);
+                })
+            },
+            onError: function(errorCode) {
+                console.log(errorCode.getErrorCode() + " " + errorCode.getErrorMessage());
+            }
+        });
+    </script>
+</body>
+<html>
---
+++ PHP/express.config.php
@@ -0,0 +1,6 @@
+<?php
+$merchantId="YOUR_SELLER_ID"; // SellerID
+$accessKey="YOUR_MWS_ACCESS_KEY"; // MWS Access Key
+$secretKey="YOUR_MWS_SECRET_KEY"; // MWS Secret Key
+$lwaClientId ="YOUR_LOGIN_WITH_AMAZON_CLIENT_ID"; // Login With Amazon Client ID
+?>
