47eef00a87799b026a54f3f23eeac3601c55ee8f
khoja-Amazon <khoja@amazon.com>
2015-01-20 13:38:48 -0800

    Comments Added
    Comments updated and formatted for RDoc

--- ruby/app/controllers/home_controller.rb
+++ ruby/app/controllers/home_controller.rb
@@ -4,28 +4,22 @@ class HomeController < ApplicationController
 
   def signature
 
-    ##
-    # Pay with Amazon Express Payments Demo
-    # Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-    # Licensed under the Apache License, Version 2.0 (the "License");
-    ##
+    # = Pay with Amazon Express Payments Demo
+    # * Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+    # * Licensed under the Apache License, Version 2.0 (the "License");
 
-    ##
     # You may not use this file except in compliance with the License. You may obtain
     # a copy of the License at:
-    # @see http://aws.amazon.com/apache2.0/
+    # * @see http://aws.amazon.com/apache2.0/
     # or in the "license" file accompanying this file. This file is distributed on an
     # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
     # implied. See the License for the specific language governing permissions and
     # limitations under the License.
-    ##
 
     require 'express_amazon_payments.rb'
 
-    ##
     # Mandatory fields
     # fields are defined in config/secrets.yml
-    ##
 
     access_key = Rails.application.secrets.access_key
     secret_key = Rails.application.secrets.secret_key
@@ -34,10 +28,8 @@ class HomeController < ApplicationController
     return_url = Rails.application.secrets.return_url
     lwa_client_id = Rails.application.secrets.lwa_client_id
 
-    ##
     # Optional fields
     # fields are defined in config/secrets.yml
-    ##
 
     currency_code = params[:payment_currency].to_s
     seller_note = params[:seller_note].to_s
@@ -45,19 +37,17 @@ class HomeController < ApplicationController
     shipping_address_required = Rails.application.secrets.shipping_address_required.to_s
     payment_action = Rails.application.secrets.payment_action
 
-    ##
     # Initialize Express Amazon Payments client
-    #
-    ##
 
-    client = ExpressAmazonPayments::Client.new(access_key, secret_key, seller_id, amount, return_url, lwa_client_id, currency_code: currency_code, seller_note: seller_note, seller_order_id: seller_order_id, shipping_address_required: shipping_address_required, payment_action: payment_action)
+    client = ExpressAmazonPayments::Client.new(
+    access_key, secret_key, seller_id, amount, return_url, lwa_client_id,
+    currency_code: currency_code, seller_note: seller_note, seller_order_id: seller_order_id,
+    shipping_address_required: shipping_address_required, payment_action: payment_action
+    )
 
-    ##
     # Run client. This output will be in json format. The page will now render json. No view is required for this method.
-    #
-    ##
 
-    render :json => client.run!
+    render :json => client.signature
 
   end
 
--- ruby/lib/express_amazon_payments.rb
+++ ruby/lib/express_amazon_payments.rb
@@ -4,40 +4,34 @@ require 'openssl'
 
 module ExpressAmazonPayments
 
-  ##
-  # Pay with Amazon Express Payments Demo
-  # Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-  # Licensed under the Apache License, Version 2.0 (the "License");
-  ##
 
-  ##
+  # = Pay with Amazon Express Payments Demo
+  # * Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
+  # * Licensed under the Apache License, Version 2.0 (the "License");
+
   # You may not use this file except in compliance with the License. You may obtain
   # a copy of the License at:
-  # @see http://aws.amazon.com/apache2.0/
+  # * @see http://aws.amazon.com/apache2.0/
   # or in the "license" file accompanying this file. This file is distributed on an
   # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
   # implied. See the License for the specific language governing permissions and
   # limitations under the License.
-  ##
 
   class Client
 
-    ##
     # Seller keys will be available at:
-    # @see https://sellercentral.amazon.com
-    # @param access_key [string]
-    # @param secret_key [string]
-    # @param seller_id [string]
-    # @param amount [string]
-    # @param return_url [string]
-    # @param lwa_client_id [string]
-    # @optional currency_code [string] default: USD
-    # @optional payment_action [string] default: Authorize
-    # @optional seller_note [string] default:
-    # @optional seller_order_id [string] default:
-    # @optional shipping_address_required [string] default: true
-    #
-    ##
+    # * @see https://sellercentral.amazon.com
+    # * @param access_key [string]
+    # * @param secret_key [string]
+    # * @param seller_id [string]
+    # * @param amount [string]
+    # * @param return_url [string]
+    # * @param lwa_client_id [string]
+    # * @optional currency_code [string] default: USD
+    # * @optional payment_action [string] default: Authorize
+    # * @optional seller_note [string] default:
+    # * @optional seller_order_id [string] default:
+    # * @optional shipping_address_required [string] default: true
 
     def initialize(access_key, secret_key, seller_id, amount, return_url, lwa_client_id, currency_code: 'USD', payment_action: 'Authorize', seller_note: ' ', seller_order_id: ' ', shipping_address_required: 'true')
       @access_key = access_key
@@ -55,13 +49,10 @@ module ExpressAmazonPayments
     end
 
 
-    def run!
+    def signature
 
-      ##
       # Build hash to generate signature
       # Current order is mandatory
-      #
-      ##
 
       parameters = {
         :accessKey => @access_key,
@@ -76,25 +67,11 @@ module ExpressAmazonPayments
         :shippingAddressRequired => @shipping_address_required
       }
 
-      ##
-      # Calling function to sign the paramters and return the URL encoded signature
-      #
-      #
-      ##
+      # Sign the parameters and return the URL-encoded signatre
 
       signature = urlencode(sign_parameters(parameters))
 
-      ##
-      # Add the signature to the paramters hash
-      #
-      ##
-
       parameters[:signature] = signature
-
-      ##
-      # Convert hash to json object
-      #
-      ##
 
       parameters.to_json
 
@@ -103,11 +80,7 @@ module ExpressAmazonPayments
 
     private
 
-    ##
     # Sets the encoding algorithm and creates a concatenated string from the parmaeters hash
-    #
-    ##
-
 
     def sign_parameters(parameters)
       string_to_sign = nil
@@ -117,10 +90,7 @@ module ExpressAmazonPayments
       return sign(string_to_sign, algorithm)
     end
 
-    ##
-    # Creates the string from the parameters hash with added values
-    #
-    ##
+    # Adds headers string to concatenated parameters string
 
     def calculate_string_to_sign(parameters)
       data = "POST"
@@ -134,10 +104,7 @@ module ExpressAmazonPayments
       return data
     end
 
-    ##
-    # URL encodes the parameters hash and creates a string
-    #
-    ##
+    # URL-encodes the parameters hash values and creates a concatenated string
 
     def get_parameters_as_string(parameters)
       query_parameters = []
@@ -148,19 +115,13 @@ module ExpressAmazonPayments
       return query_parameters.join('&')
     end
 
-    ##
     # URL encodes the hash values
-    #
-    ##
 
     def urlencode(value)
       return CGI.escape(value).gsub('+','%20')
     end
 
-    ##
     # Generates the signature that will be added back to the parameters hash
-    #
-    ##
 
     def sign(data, algorithm)
       if algorithm == 'HmacSHA1'
