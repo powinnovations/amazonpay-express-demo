3fd6363e7abc3ac54589902b766b7f3b5626a9c8
khoja-Amazon <khoja@amazon.com>
2015-01-20 16:47:41 -0800

    Comments Updated
    Comments cleaned up

--- ruby/app/controllers/home_controller.rb
+++ ruby/app/controllers/home_controller.rb
@@ -2,25 +2,16 @@ class HomeController < ApplicationController
   def index
   end
 
+  # = Pay with Amazon Express Payments Demo
+  # The signature method sets the required and optional parameter values and
+  # initializes the Express Amazon Payments client. The method is
+  # rendered in json format. A view is not needed for this method. 
   def signature
-
-    # = Pay with Amazon Express Payments Demo
-    # * Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-    # * Licensed under the Apache License, Version 2.0 (the "License");
-
-    # You may not use this file except in compliance with the License. You may obtain
-    # a copy of the License at:
-    # * @see http://aws.amazon.com/apache2.0/
-    # or in the "license" file accompanying this file. This file is distributed on an
-    # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-    # implied. See the License for the specific language governing permissions and
-    # limitations under the License.
 
     require 'express_amazon_payments.rb'
 
     # Mandatory fields
     # fields are defined in config/secrets.yml
-
     access_key = Rails.application.secrets.access_key
     secret_key = Rails.application.secrets.secret_key
     seller_id = Rails.application.secrets.seller_id
@@ -30,7 +21,6 @@ class HomeController < ApplicationController
 
     # Optional fields
     # fields are defined in config/secrets.yml
-
     currency_code = params[:payment_currency].to_s
     seller_note = params[:seller_note].to_s
     seller_order_id = Rails.application.secrets.seller_order_id
@@ -38,14 +28,11 @@ class HomeController < ApplicationController
     payment_action = Rails.application.secrets.payment_action
 
     # Initialize Express Amazon Payments client
-
     client = ExpressAmazonPayments::Client.new(
     access_key, secret_key, seller_id, amount, return_url, lwa_client_id,
     currency_code: currency_code, seller_note: seller_note, seller_order_id: seller_order_id,
     shipping_address_required: shipping_address_required, payment_action: payment_action
     )
-
-    # The output will be in json format. The page will render json. No view is required for this method.
 
     render :json => client.signature
 
--- ruby/lib/express_amazon_payments.rb
+++ ruby/lib/express_amazon_payments.rb
@@ -4,19 +4,10 @@ require 'openssl'
 
 module ExpressAmazonPayments
 
-
-  # = Pay with Amazon Express Payments Demo
-  # * Copyright 2015 Amazon.com, Inc. or its affiliates. All Rights Reserved.
-  # * Licensed under the Apache License, Version 2.0 (the "License");
-
-  # You may not use this file except in compliance with the License. You may obtain
-  # a copy of the License at:
-  # * @see http://aws.amazon.com/apache2.0/
-  # or in the "license" file accompanying this file. This file is distributed on an
-  # "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or
-  # implied. See the License for the specific language governing permissions and
-  # limitations under the License.
-
+  # = Pay with Amazon - Express Payments - Demo
+  # This client allows a seller to dynamically integrate with Express Payments by passing in the
+  # required keys and adjustable parameter values (amount, seller_note, seller_order_id, shipping_address_required).
+  # The client then generates a signature that is used to make the Express Payment request.
   class Client
 
     # Seller keys will be available at:
@@ -32,7 +23,6 @@ module ExpressAmazonPayments
     # * @optional seller_note [string] default:
     # * @optional seller_order_id [string] default:
     # * @optional shipping_address_required [string] default: true
-
     def initialize(access_key, secret_key, seller_id, amount, return_url, lwa_client_id, currency_code: 'USD', payment_action: 'Authorize', seller_note: ' ', seller_order_id: ' ', shipping_address_required: 'true')
       @access_key = access_key
       @secret_key = secret_key
@@ -45,15 +35,14 @@ module ExpressAmazonPayments
       @seller_note = seller_note
       @seller_order_id = seller_order_id
       @shipping_address_required = shipping_address_required
-
     end
 
-
+    # The signature method creates the hash with all parameter values.
+    # The hash is used to URL-encode the values and generate the signatre.
+    # A json formated string is returned.
     def signature
 
       # Build hash to generate signature
-      # Current order is mandatory
-
       parameters = {
         :accessKey => @access_key,
         :amount => @amount,
@@ -68,20 +57,15 @@ module ExpressAmazonPayments
       }
 
       # Sign the parameters and return the URL-encoded signatre
-
       signature = urlencode(sign_parameters(parameters))
-
       parameters[:signature] = signature
-
       parameters.to_json
-
     end
 
 
     private
 
     # Sets the encoding algorithm and creates a concatenated string from the parmaeters hash
-
     def sign_parameters(parameters)
       string_to_sign = nil
       algorithm    = "HmacSHA256"
@@ -91,7 +75,6 @@ module ExpressAmazonPayments
     end
 
     # Adds headers string to concatenated parameters string
-
     def calculate_string_to_sign(parameters)
       data = "POST"
       data += "\n"
@@ -105,7 +88,6 @@ module ExpressAmazonPayments
     end
 
     # URL-encodes the parameters hash values and creates a concatenated string
-
     def get_parameters_as_string(parameters)
       query_parameters = []
       parameters.each { |key,value|
@@ -116,13 +98,11 @@ module ExpressAmazonPayments
     end
 
     # URL encodes the hash values
-
     def urlencode(value)
       return CGI.escape(value).gsub('+','%20')
     end
 
     # Generates the signature that will be added back to the parameters hash
-
     def sign(data, algorithm)
       if algorithm == 'HmacSHA1'
         hash = 'sha1'
@@ -134,7 +114,6 @@ module ExpressAmazonPayments
       digest = OpenSSL::HMAC.digest(hash, @secret_key, data)
       return Base64.strict_encode64(digest)
     end
-
 
   end
 
