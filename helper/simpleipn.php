82f2fa393ba2fe867a39c2cd9bd94700f8c65e4c
Brent Douglas <dbrent@amazon.com>
2015-08-11 13:55:21 -0700

    Update SimpleIPN.

--- helper/simpleipn.php
+++ helper/simpleipn.php
@@ -4,6 +4,11 @@
  * SimpleIPN - This script will verify, parse, and email IPN data. This
  * script will only recognize IPNs generated from an Express button. Your
  * server must be configured to send email (e.g. sendmail).
+ *
+ * This file should be copied to a location on your web server that is
+ * publicly accessible. You will also need to configure your "Merchant URL"
+ * in Seller Central via Settings > Integration Settings. Set it to the
+ * location of this script.
  */
 
 // Who do you want emails to be sent to?
